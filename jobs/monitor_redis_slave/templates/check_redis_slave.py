082e0f974be08187a7a3c556934e2c784fde935c
Hussain, Bilal <bilal.hussain@sky.uk>
2017-10-25 11:03:38 +0100

    adding monitor-redis bosh release

---
+++ jobs/monitor_redis_slave/templates/check_redis_slave.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python
+
+# Redis Slave Connectivity Check
+
+import os, sys, json
+sys.path.insert(0, '/var/vcap/packages/python-2.7/lib/python2.7/site-packages')
+import redis
+
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+
+host = "<%= p('redis_slave.host') %>"
+port = "<%= p('redis_slave.port') %>"
+password = "<%= p('redis_slave.password') %>"
+
+def conn_redis():
+    global host, port, password
+    try:
+        conn = redis.StrictRedis(host, port, db=0, password=password)
+        conn.ping()
+    except redis.ConnectionError:
+        print "Unknown: Redis connection is failed."
+        sys.exit(1)
+    return conn
+
+def valid_redis():
+    r = conn_redis()
+    i = r.info()
+    r_out = json.loads(json.dumps(i))
+    r_role = r_out["role"]
+    if r_role != 'slave':
+      print 'Unknown: target redis server must be a slave!'
+      sys.exit(1)
+
+if __name__ == '__main__':
+    valid_redis()
+
+def check_redis():
+    r = conn_redis()
+    i = r.info()
+    r_out = json.loads(json.dumps(i))
+    r_link = r_out["master_link_status"]
+    r_iosec = r_out["master_last_io_seconds_ago"]
+    if r_link != 'up' and r_iosec == -1:
+       print 'Critical: slave redis server is failed to connect master.It is down!'
+       sys.exit(1)
+    else:
+       print 'OK: slave redis is connected properly to master'
+       sys.exit(0)
+
+if __name__ == '__main__':
+    check_redis()
